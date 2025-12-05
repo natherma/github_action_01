4597ebf099cfa2612648186dad49379aed632cff
natherma <nathermanafees786@gmail.com>
2025-12-06 01:29:01 +0530

    second commit

---
+++ test/test_operation.py
@@ -0,0 +1,14 @@
+from src.math import add,sub
+
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+def tes_sub():
+    assert sub(1,1)==0
+    assert sub(5,1)==4
+    
+    
+
+    
